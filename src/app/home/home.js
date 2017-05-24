2ae7adbac454033f91328c068b5596a451ca2037
shivangibilora13 <shivangi.bilora@delhivery.com>
2017-05-24 23:57:06 +0530

    update: home module to include search

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -16,8 +16,16 @@ angular.module("eShop")
     /**
      * Method to fetch products
      **/
-    scope.getProducts = function() {
-        return $http.get('http://localhost:3000/products');
+    scope.getProducts = function(q) {
+        q = q ? q : '';
+        return $http.get('http://localhost:3000/products?q=' + q);
+    };
+
+    /**
+     * Method to add products to cart
+     **/
+    scope.addToCart = function(product) {
+        return $http.post('http://localhost:3000/cart', product);
     };
 })
 
@@ -37,18 +45,34 @@ angular.module("eShop")
             });
     };
 
+    $scope.filterProducts = function() {
+        homeService.getProducts($scope.query)
+            .then(function(res) {
+                $scope.filteredProducts = res.data;
+            }, function(err) {
+                alert("Oops..!!! Seems like we were not able to fetch products.");
+            });
+    };
+
+    $scope.addToCart = function(product) {
+        homeService.addToCart(product)
+            .then(function(res) {
+                alert("Successfully added to cart");
+            });
+    };
+
     $scope.moveToPrev = function() {
-    	$scope.selectedItem -= 1;
-    	if($scope.selectedItem < 0){
-    		$scope.selectedItem = $scope.products.length-1;
-    	}
+        $scope.selectedItem -= 1;
+        if ($scope.selectedItem < 0) {
+            $scope.selectedItem = $scope.products.length - 1;
+        }
     };
 
     $scope.moveToNext = function() {
-    	$scope.selectedItem += 1;
-    	if($scope.selectedItem > $scope.products.length-1){
-    		$scope.selectedItem = 0;
-    	}
+        $scope.selectedItem += 1;
+        if ($scope.selectedItem > $scope.products.length - 1) {
+            $scope.selectedItem = 0;
+        }
     };
 
     init();
