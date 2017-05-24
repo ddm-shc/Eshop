947ce65be73633d8af561ea7f907167b8b4bbd05
shivangibilora13 <shivangi.bilora@delhivery.com>
2017-05-24 23:19:50 +0530

    update: home module to include carousel and render products

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -9,6 +9,47 @@ angular.module("eShop")
         });
 })
 
-.controller("HomeCtrl", function($scope) {
-    $scope.title = "Hello";
+.service("homeService", function($http, $q) {
+
+    var scope = this;
+
+    /**
+     * Method to fetch products
+     **/
+    scope.getProducts = function() {
+        return $http.get('http://localhost:3000/products');
+    };
+})
+
+.controller("HomeCtrl", function($scope, homeService) {
+
+    var init = function() {
+        $scope.getProducts();
+        $scope.selectedItem = 0;
+    }
+
+    $scope.getProducts = function() {
+        homeService.getProducts()
+            .then(function(res) {
+                $scope.products = res.data;
+            }, function(err) {
+                alert("Oops..!!! Seems like we were not able to fetch products.");
+            });
+    };
+
+    $scope.moveToPrev = function() {
+    	$scope.selectedItem -= 1;
+    	if($scope.selectedItem < 0){
+    		$scope.selectedItem = $scope.products.length-1;
+    	}
+    };
+
+    $scope.moveToNext = function() {
+    	$scope.selectedItem += 1;
+    	if($scope.selectedItem > $scope.products.length-1){
+    		$scope.selectedItem = 0;
+    	}
+    };
+
+    init();
 });
